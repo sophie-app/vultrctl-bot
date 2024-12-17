bc6ae952ff0462ffc4e2708e1c4118d76b118117
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-12-17 22:35:49 +0900

    improve: prevent command timeout by resDefer

--- src/commands/up.ts
+++ src/commands/up.ts
@@ -3,19 +3,21 @@ import type { Command } from '.'
 import type { Env } from '..'
 import { createVultrClient } from '../lib/vultr/vultrClient'
 
-const upCommandHandler = async (c: CommandContext<Env>) => {
+const upCommandHandler = (c: CommandContext<Env>) => {
   const vultrClient = createVultrClient(c.env.VULTR_API_KEY, c.env.VULTR_API_BASEURL)
   const instanceId = c.env.VULTR_INSTANCE_ID
 
-  const { error } = await vultrClient.POST('/instances/{instance-id}/start', {
-    params: { path: { 'instance-id': instanceId } },
+  return c.resDefer(async (c) => {
+    const { error } = await vultrClient.POST('/instances/{instance-id}/start', {
+      params: { path: { 'instance-id': instanceId } },
+    })
+
+    if (error !== undefined) {
+      return c.followup(`❎ failed to start instance ${error}`)
+    }
+
+    return c.followup('✅ instance started')
   })
-
-  if (error !== undefined) {
-    return c.res(`failed to start instance ${error}`)
-  }
-
-  return c.res('✅ instance started')
 }
 
 export const upCommand: Command = {
